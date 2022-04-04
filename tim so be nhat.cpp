fcb8e740960d5477fea47794c5e304df20ac775c
cuongkq5555 <khuongquycuong@gmail.com>
2022-04-04 18:10:53 +0700

    Create tim so be nhat.cpp

---
+++ tim so be nhat.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main(){
+	// input a
+	int a;
+	scanf("%d",&a);
+	// input b
+	int b;
+	scanf("%d",&b);
+	int c;
+	scanf("%d",&c);
+	if(a<b){
+		if(a<c){
+			printf("a la so be nhat");
+		}else{
+			printf("c la so be nhat");
+		}
+	}
+	else{if(b<c){
+		printf("b la so be nhat");
+	}else{
+		printf("c la so be nhat");
+	}
+	}
+	
+	
+	
+	
+	
+	
+}
